5c6df6695c1677be5a3d0089215378cd7af9c5dd
Adrien Lawrence <firestar9jr@gmail.com>
2025-08-27 16:22:45 -0400

    Fix: resolved separation of concerns
    Created BillView class in new view package to use getText(), which
    displays the bill items and totals. Subsequently removed getText() from
    Bill class and created a new getItems() method to return the list of
    bill items in the new BillView class

---
+++ code/Lab 1/src/edu/westga/cs1302/lab1/view/BillView.java
@@ -0,0 +1,59 @@
+package edu.westga.cs1302.lab1.view;
+
+//Importing the Bill to view
+
+import edu.westga.cs1302.lab1.model.Bill;
+
+/**This class is responsible for displaying 
+ * the Bill object, as previously handled by the 
+ * Bill class
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class BillView {
+
+	private Bill billDisplay;
+	
+	/**Defines the new billDisplay object 
+	 * to be used in the BillView class
+	 * 
+	 * @precondition billDisplay != null
+	 * @postcondition bill argument is assigned to BillDisplay
+	 * 
+	 * @param bill the bill to be assigned
+	 */
+	public BillView(Bill bill) {
+		if (bill == null) {
+			throw new IllegalArgumentException("Bill cannot be null");
+		}
+		this.billDisplay = bill;
+	}
+	
+	/** Return a String containing the list of bill items and total for the bill.
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return a String containing the list of bill items and total for the bill
+	 */
+	public String getText() {
+		String text = "ITEMS" + System.lineSeparator();
+		double subTotal = 0.0;
+		for (var item : this.billDisplay.getItems()) {
+			text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
+			subTotal += item.getAmount();
+		}
+		
+		text += System.lineSeparator();
+		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
+		double tax = subTotal * 0.1;
+		double tip = subTotal * 0.2;
+		text += "TAX - $" + tax + System.lineSeparator();
+		text += "TIP - $" + tip + System.lineSeparator();
+		text += "TOTAL - $" + (subTotal + tip + tax);
+		
+		return text;
+	}
+	
+}
