4ba96a3ea6b6d4cca00b2751b6ff20010230dfda
Adrien Lawrence <firestar9jr@gmail.com>
2025-09-03 22:20:15 -0400

    Implemented TestConstructor class to test BillItem
    Added test cases for the BillItem constructor which test the
    preconditions for the parameters 'name' and 'amount'

---
+++ code/starterLab2/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill_item/TestConstructor.java
@@ -0,0 +1,42 @@
+package edu.westga.cs1302.lab2.tests.model.bill_item;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab2.model.BillItem;
+
+class TestConstructor {
+	
+	@Test
+	public void itemTest() {
+		
+		BillItem item = new BillItem("item", 2.50);
+		
+		assertEquals(2.50, item.getAmount());
+		assertEquals("item", item.getName());
+	}
+	
+	@Test
+	public void nullNameTest() {
+		
+		assertThrows(IllegalArgumentException.class, () -> new BillItem(null, 9.40));
+		
+	}
+	
+	@Test
+	public void negativeAmount() { 
+		
+		assertThrows(IllegalArgumentException.class, () -> new BillItem("item", -3.50));
+	}
+	
+	@Test
+	public void zeroAmount() {
+		
+		assertThrows(IllegalArgumentException.class, () -> new BillItem("item", 0));
+		
+	}
+
+}
