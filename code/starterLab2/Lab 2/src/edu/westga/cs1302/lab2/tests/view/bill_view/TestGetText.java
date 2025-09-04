2a951e33d30409bb284aab58c186d4ab0085b674
Adrien Lawrence <firestar9jr@gmail.com>
2025-09-03 23:26:28 -0400

    Implemented TestGetText class to test BillView
    Added test cases into TestGetText to test normal operations of the
    getText method in the BillView class. Added a test case with an empty
    bill to test proper bill total and subtotals of zero dollars.

---
+++ code/starterLab2/Lab 2/src/edu/westga/cs1302/lab2/tests/view/bill_view/TestGetText.java
@@ -0,0 +1,65 @@
+package edu.westga.cs1302.lab2.tests.view.bill_view;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+import edu.westga.cs1302.lab2.view.BillView;
+
+class TestGetText {
+	
+	@Test
+	public void billItemsTest() {
+		Bill bill = new Bill();
+		
+		BillItem item1 = new BillItem("Item1", 2.50);
+		BillItem item2 = new BillItem("Item2", 3.50);
+		
+		bill.addItem(item1);
+		bill.addItem(item2);
+		
+		BillView view = new BillView();
+		String result = view.getText(bill);
+		
+		assertTrue(result.contains("Item2"));
+		assertTrue(result.contains("Item1"));
+		assertTrue(result.contains("2.50"));
+		assertTrue(result.contains("3.50"));
+		
+	}
+	
+	@Test
+	public void totalsTest() {
+		Bill bill = new Bill();
+		
+		BillItem item1 = new BillItem("Item1", 2.50);
+		BillItem item2 = new BillItem("Item2", 3.50);
+		
+		bill.addItem(item1);
+		bill.addItem(item2);
+		
+		BillView view = new BillView();
+		String result = view.getText(bill);
+		
+		//1.3 = 1 (subtotal) + .1 (tax) + .2 (tip)
+		double total = (2.50 + 3.50) * 1.30;
+		
+		//Assuming doubles are represented like this
+		assertTrue(result.contains("SUBTOTAL - $6.0"));
+		assertTrue(result.contains("TOTAL - $" + total));
+	}
+	
+	@Test
+	public void emptyBillTest() {
+		Bill bill = new Bill();
+		
+		BillView view = new BillView();
+		String result = view.getText(bill);
+		
+		assertTrue(result.contains("SUBTOTAL - $0.0"));
+		assertTrue(result.contains("TOTAL - $0.0"));
+	}
+
+}
