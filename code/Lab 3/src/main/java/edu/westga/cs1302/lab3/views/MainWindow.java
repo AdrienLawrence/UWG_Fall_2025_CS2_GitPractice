4730cd2bca7eea7fd2d87dd9daaf21db39e64a5f
Adrien Lawrence <firestar9jr@gmail.com>
2025-09-08 12:48:00 -0400

    Modified MainWindow.java and MainWindow.fxml
    Added GUI elements into MainWindow.fxml and added the resulting code
    skeleton into MainWindow.java to be implemented with methods.

--- code/Lab 3/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/Lab 3/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,6 +1,11 @@
 package edu.westga.cs1302.lab3.views;
 
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 /**
  * Controller class for drawing various things to our canvas window.
  * 
@@ -9,10 +14,27 @@ package edu.westga.cs1302.lab3.views;
  */
 public class MainWindow {
     
-    /**
-     * Perform any needed initialization of UI components and underlying objects.
-     */
-    public void initialize() {
-    	
+    @FXML
+    private Button addItemButton;
+
+    @FXML
+    private TextField amountField;
+
+    @FXML
+    private Text amountText;
+
+    @FXML
+    private TextArea billDisplay;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Text nameText;
+
+    @FXML
+    void addItem(ActionEvent event) {
+
     }
+
 }
