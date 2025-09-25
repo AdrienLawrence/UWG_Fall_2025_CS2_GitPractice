d3a0c81a8feba63e5292e2fe8f228863a446dc93
Adrien Lawrence <firestar9jr@gmail.com>
2025-09-25 16:53:57 -0400

    1B Completed: Task.java and TestConstructor.java
    Implemented Task.java with instance variables, a constructor, and
    necessary getter functions as specified by the assignment.
    TestConstructor.java includes precondition check cases for the
    constructor. Checkstyle warnings will not be addressed until final
    commits so there are missing javadoc comments

--- code/Project 1/src/main/java/edu/westga/cs1302/project1/model/Task.java
+++ code/Project 1/src/main/java/edu/westga/cs1302/project1/model/Task.java
@@ -1,5 +1,47 @@
 package edu.westga.cs1302.project1.model;
 
 public class Task {
+	
+	private final String name;
+	
+	private String description;
+	
+	private final int priority;
 
+	public Task(String n, String d, int p) {
+		
+		if (n == null || n.isEmpty() || n.trim().isEmpty()) {
+			throw new IllegalArgumentException("Name cannot be null or blank");
+		}
+		
+		if (d == null || d.trim().isEmpty()) {
+			throw new IllegalArgumentException("Description cannot be null or blank");
+		}
+		
+		if (p < 1) {
+			throw new IllegalArgumentException("Priority must be a chosen number greater than 0");
+		}
+		
+		this.name = n;
+		this.description = d;
+		this.priority = p;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public int getPriority() {
+		return this.priority;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
 }
--- code/Project 1/src/test/java/edu/westga/cs1302/project1/model/task/TestConstructor.java
+++ code/Project 1/src/test/java/edu/westga/cs1302/project1/model/task/TestConstructor.java
@@ -4,11 +4,73 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.project1.model.Task;
+
 class TestConstructor {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+    @Test
+    void validParamTest() {
+        Task task = new Task("Study", "Study for exam", 1);
+        
+        assertEquals("Study", task.getName());
+        assertEquals("Study for exam", task.getDescription());
+        assertEquals(1, task.getPriority());
+    }
+    
+    @Test
+    void nameReturnTest() {
+        Task task = new Task("Homework", "Math assignment", 2);
+        assertEquals("Homework", task.toString());
+    }
+    
+    @Test
+    void nullNameTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Task(null, "Valid description", 1);
+        });
+    }
+    
+    @Test
+    void blankNameTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Task("   ", "desc", 1);
+        });
+        
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Task("", "desc", 1);
+        });
+    }
+    
+    @Test
+    void nullDescTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Task("name", null, 1);
+        });
+    }
+    
+    @Test
+    void blankDescTest() {
+    	assertThrows(IllegalArgumentException.class, () -> {
+    		new Task("name", "", 1);
+    	});
+    	
+    	assertThrows(IllegalArgumentException.class, () -> {
+    		new Task("name", "  ", 1);
+    	});
+    }
+    
+    @Test
+    void zeroAsPrioTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Task("name", "desc", 0);
+        });
+    }
+    
+    @Test
+    void negativePrioTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Task("name", "desc", -5);
+        });
+    }
+	
 }
